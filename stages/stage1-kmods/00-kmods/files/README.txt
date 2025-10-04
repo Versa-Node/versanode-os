486aa83e961b2e9e10359500206b3f5b32e62b0f
Pieter Cawood <pieter.cawood@gmail.com>
2025-10-04 14:22:56 +0200

    Untrack stage1-kmods to convert into submodule

--- stages/stage1-kmods/00-kmods/files/README.txt
+++
@@ -1,1 +0,0 @@
-Put any files you want copied by 00-run.sh in this directory.
--- stages/stage1-kmods/prerun.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -e
-# Every custom stage must copy the previous stage rootfs so the next stage sees it.
-if [ ! -d "${ROOTFS_DIR}" ]; then
-  copy_previous
-fi
